013e70d454f94a8f69b458e13d1d1b0e0a1bbe13
alnicolaou96 <alnicolaou96@gmail.com>
2018-07-01 19:30:01 -0400

    javascript

--- AlexPortfolioSite/JavaScript.js
+++ AlexPortfolioSite/JavaScript.js
@@ -1,4 +1,8 @@
-﻿function github()
+﻿function hover()
 {
-    document.getElementsByClassName("villa").style.display = "none";
+    var x = document.getElementsByClassName("contact-methods")[0];
+    var y = document.getElementsByClassName("contact-methods")[1];
+    var z = document.getElementsByClassName("contact-methods")[2];
+    x.style.fontSize = "64px";
+    z.style.fontSize = "64px";
 }
